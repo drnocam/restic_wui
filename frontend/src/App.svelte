c3320b1808321dc9cffb5d1a3b29b92c4e56424d
okul.masaüstü <omernuricam@gmail.com>
2022-10-11 16:48:07 +0300

    bootstrap and icons added

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -1,60 +1,14 @@
 <script lang="ts">
 
-  import { onMount, text } from 'svelte/internal';
-import {CmdCheck, ChooseRepository,GetSnapshots, ReadWriteSettings} from '../wailsjs/go/main/App.js'
+import NavUrls from './NavUrls.svelte';
 
-
-let cmd_ready : boolean = false;
-let selected_repository: string = ""
-
-  let name: string
-  let repo_password: string
-  let text_result: string = ""
-
-
-
-  function choose_repository():void {
-    ChooseRepository().then(r=>selected_repository = 'Selected repository: ' + r)
-  }
-
-  function get_snapshots() :void {
-    GetSnapshots(repo_password).then(r=>{
-      let onceki = text_result;
-      text_result = r+ onceki}
-      )
-  }
-
-function read_write_settings() : void {
-
-  ReadWriteSettings().then(r=>{
-    console.log(r)
-  })
-
-}
-
-  onMount(()=>{
-    CmdCheck().then(r=>JSON.parse(r)).then(r=>{
-      if("data" in r){
-        cmd_ready = r.data== "1" ? true : false ; 
-      }       
-    })
-  })
 
 </script>
 
 <main>
-  <button class="btn" on:click={read_write_settings}>Read Write Settings</button>
 
-  <div></div>
-    <input type="password" placeholder="Write Repository Password" bind:value={repo_password} />
-    <button class="btn" on:click={choose_repository}>Choose Repository</button>
-    <div class="result" id="result">{selected_repository}</div>
-    {#if selected_repository && repo_password}
-    <button class="btn" on:click={get_snapshots}>Get Snapshots</button>
-    {:else}
-    <div>Please write password first to get snapshots.</div>
-    {/if}
-    <div style="background-color: white;min-height:200px;color:black">{text_result}</div>
+  <NavUrls />
+  
 </main>
 
 <style>
---
+++ frontend/src/NavUrls.svelte
@@ -0,0 +1,20 @@
+<script>
+import {Router, Route, Link} from 'svelte-routing'
+import Index from './page/Index.svelte';
+    import Settings from './page/Settings.svelte';
+
+</script>
+
+<style>
+    /* your styles go here */
+</style>
+<Router >
+    <nav>
+      <Link to="/">Home</Link>
+      <Link to="settings">Settings</Link>
+    </nav>
+    <div>
+     <Route path="/"><Index /></Route>
+     <Route path="settings"><Settings /></Route>
+    </div>
+  </Router>
